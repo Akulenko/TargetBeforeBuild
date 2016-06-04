86287fa30183f230861be0fa4f8305eb00ee92c8
Akulenko <atanzilya@yahoo.co.uk>
2016-06-04 22:34:00 +0300

    Add project

---
+++ TxtFile/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TxtFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string way = "";
+            int number = 10;
+
+            while (way == "")
+            {
+                Console.Write("Enter the way to the text file: ");
+                way = Console.ReadLine();
+            }
+            Console.Write("Enter the number of test-cases: ");
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("By default number of test cases is 10", e);
+            }
+            ReadingFile(way, number);
+            Console.Read();
+        }
+
+        public static void ReadingFile(string way, int number)
+        {
+            string file = "";
+            int n = 0;
+            try
+            {
+                StreamReader StreamReader = new StreamReader(way);
+                while (n <= number)
+                {
+                    file += StreamReader.ReadLine();
+                    n++;
+                }
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("Could not find file", e);
+            }
+            Console.Write(file);
+        }
+    }
+}
